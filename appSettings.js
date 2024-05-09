d48e28896830e47961017f5ce28363ab46220a08
kraxnerh19 <hannes.kraxner@edu.fh-joanneum.at>
2024-05-09 18:51:58 +0200

    First commit

---
+++ appSettings.js
@@ -0,0 +1,14 @@
+const settings = {
+    'clientId': '3af28572-7e8a-49b3-b918-46815ea2bea1',
+    'tenantId': 'common',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send',
+      'calendars.read',
+      'calendars.readwrite'
+    ]
+  };
+  
+  module.exports = settings;
+  
